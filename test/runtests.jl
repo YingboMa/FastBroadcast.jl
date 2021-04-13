75623c0adba1139c963a235bf59c1d3e53fd72cb
Yingbo Ma <mayingbo5@gmail.com>
2021-04-12 20:22:36 -0400

    Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using FastBroadcast
 using Test
 
 @testset "FastBroadcast.jl" begin
-    # Write your tests here.
+    x, y = [1,2,3,4], [5,6,7,8]
+    dst = zeros(Int, 4)
+    bc = Broadcast.Broadcasted(+, (Broadcast.Broadcasted(*, (x, y)), x, y, x, y, x, y))
+    @test FastBroadcast.fast_materialize!(dst, bc) == copy(bc)
 end
