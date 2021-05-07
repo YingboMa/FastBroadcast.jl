79c3f7a2e6f10ca2681bf4179bd3bb397814a7d8
chriselrod <elrodc@gmail.com>
2021-05-07 19:03:57 -0400

    DiffEqBase tests pass (#3)
    * DiffEqBase tests pass
    
    * Remove unused functions
    
    * Add a few tests, bump version.
    
    * More tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,4 +13,17 @@ using Test
     @test (@.. dest += (x*y) + x + y + x + y + x + y) ≈ 2bcref
     @test (@.. dest -= (x*y) + x + y + x + y + x + y) ≈ bcref
     @test (@.. dest *= (x*y) + x + y + x + y + x + y) ≈ abs2.(bcref)
+    @test (@.. (x*y) + x + y + x*(3,4,5,6) + y + x * (1,) + y + 3) ≈ (@. (x*y) + x + y + x*(3,4,5,6) + y + x * (1,) + y + 3)
+    A = rand(4,4);
+    @test (@.. A * y' + x) ≈ (@. A * y' + x)
+    @test (@.. A * transpose(y) + x) ≈ (@. A * transpose(y) + x)
+    Ashim = A[1:1,:];
+    @test (@.. Ashim * y' + x) ≈ (@. Ashim * y' + x) # test fallback
+    Av = view(A,1,:);
+    @test (@.. Av * y' + A) ≈ (@. Av * y' + A)
+    B = similar(A);
+    @.. B = A
+    @test B == A
+    @.. A = 3
+    @test all(==(3), A)
 end
