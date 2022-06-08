d775d07e140e25135c45e8c68a531c41aaaafe1f
Chris Elrod <elrodc@gmail.com>
2022-06-08 11:05:48 -0400

    add docstring to @..

--- src/FastBroadcast.jl
+++ src/FastBroadcast.jl
@@ -335,6 +335,20 @@ function fb_macro(ex, mod, threadarg, broadcastarg)
     esc(broadcast_codeinfo(lowered.args[1], threadarg, broadcastarg))
 end
 
+"""
+    @.. [thread=false] [broadcast=false] expr
+
+`@..` turns `expr` into a broadcast-like expression, similar to `@.`.
+It additionally provides two optional keyword arguments:
+
+- thread: Defaults to `false`. Use multithreading?
+- broadcast: Defaults to `false`. If `true`, it will broadcast axes with dynamic
+    runtime sizes of `1` to larger sizes, if `false` only sizes known to be `1`
+    at compile time will be supported, i.e. axes such that
+    `ArrayInterface.known_length(typeof(ArrayInterface.axes(x,i))) == 1` will
+    be broadcast. Note that this differs from base broadcasting, in that
+    base broadcasting only supports `broadcast=true`.
+"""
 macro (..)(ex)
     fb_macro(ex, __module__, False(), False())
 end
