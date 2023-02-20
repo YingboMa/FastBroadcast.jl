be489a466b0c62557024bbe9f56637276b00c578
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-02-20 08:12:34 -0500

    update ArrayInterface v7 (#42)
    Co-authored-by: Chris Elrod <elrodc@gmail.com>

--- test/vectorization_tests.jl
+++ test/vectorization_tests.jl
@@ -47,7 +47,7 @@ foo26(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y,
 
 if Base.JLOptions().check_bounds != 1
     @test @allocated(foo9(a, b, c, d, e, f, g, h, i)) == 0
-    @allocated(
+    @test @allocated(
         foo26(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z)
     ) == 0
     io = IOBuffer()
